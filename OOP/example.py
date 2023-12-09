1f73c4cd4e30ceaea553ba31ccfb98b7fdcbc5ce
Rico Flores <87421261+romaricoflores@users.noreply.github.com>
2023-12-08 23:49:34 -0800

    save

--- OOP/example.py
+++ OOP/example.py
@@ -1,2 +1,10 @@
 from shirt import Shirt
 
+shirt_one = Shirt('red', 'M', 'long_sleeved', '45')
+shirt_two = Shirt('orange', 'S', 'short_sleeved', '30')
+
+print(shirt_one.price)
+print(shirt_two.color)
+
+shirt_two.change_price(59)
+print(shirt_two.price)
